1d0adedd7b7dd2ab8813a306ff797433d739cc5f
Jathmel Gordon <jathmel@gmail.com>
2018-05-03 01:46:08 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
  # your code here!
+ result = []
+ i = 0
+ while i < collection.length 
+  if yield(array[i]) == true 
+    result << array[i]
+  end
+  i += 1 
+end
+result
 end
